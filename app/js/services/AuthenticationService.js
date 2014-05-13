792454c1257c71b764a82cfd8633d3688bb8c602
joinee <bartosz.glow@gmail.com>
2014-05-13 15:15:27 +0200

    Login form with db connection

--- app/js/services/AuthenticationService.js
+++ app/js/services/AuthenticationService.js
@@ -1,15 +1,30 @@
-app.factory("AuthenticationService", ['$location', function($location) {
+app.factory("AuthenticationService", ['$location', '$http', function($location, $http) {
+
+	var userInfo = {};
 
 	return {
 		login: function(credentials) {	
+			alert(credentials.username + " " + credentials.password);
+
 	        if( credentials.username === "dispatcher" && credentials.password === "password") {
 	            console.log($location);
-	            $location.path('/map');
-	        }
-	        if( credentials.username === "ambulance" && credentials.password === "password") {
-	            console.log($location);
-	            $location.path('/ambulance');
-	        }
+	            $location.path('/map');	
+	        } else {
+	        	$http.post('/geolance/php/login', {'username': credentials.username, 'password': credentials.password})
+	        		.success(function(data, status, headers, config) {
+                        if (data.username !== '') {
+                        	userInfo = data;
+	            			$location.path('/ambulance');	
+                        }
+                        else {
+                            alert(data);
+                            console.log(data);
+                        }})
+                    .error(function(data, status) { 	// called asynchronously if an error occurs
+														// or server returns response with an error status.
+                        alert(status);
+                    });
+            }
 		},
 		logout: function() {
 
---
+++ php/login.php
@@ -0,0 +1,37 @@
+<?php
+
+	$data = json_decode(file_get_contents("php://input"));
+	$username = mysql_real_escape_string($data->username);
+	$password = mysql_real_escape_string($data->password);
+	
+	$link = mysql_connect('localhost', 'admin', '') or die('Could not connect: ' . mysql_error());
+	mysql_select_db('test') or die('Could not select database' . mysql_errno());
+	
+	$query = 'SELECT * FROM AMBULANCES WHERE username = "' . $username . '" ';
+
+	$result = mysql_query($query) or die('Query failed: ' . mysql_error());
+
+	$line = mysql_fetch_array($result, MYSQL_ASSOC);
+
+	if( $line["password"] == $password ) {	
+        $arr = array(
+        			'username' 	=> $line["username"], 
+        			'state' 	=> $line["state"], 
+        			'latitude' 	=> $line["latitude"],
+        			'longitude' => $line["longitude"],
+        			'accuracy' 	=> $line["accuracy"]
+        		);
+    	$jsn = json_encode($arr);
+    	print_r($jsn);
+	} else {
+        $arr = array('username' => "", 'error' => '');
+        $jsn = json_encode($arr);
+        print_r($jsn);
+	}
+
+	mysql_free_result($result);
+
+	// Closing connection
+	mysql_close($link);
+
+?>
