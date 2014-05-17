e442febfdf70b272802ada2a1249e93e5ed7ae8e
joinee <bartosz.glow@gmail.com>
2014-05-17 22:00:56 +0200

    map style changed

--- app/js/controllers/mapCtrl.js
+++ app/js/controllers/mapCtrl.js
@@ -10,7 +10,8 @@ app.controller('mapCtrl', ['$scope', 'ReceiveService', function ($scope, Receive
 
 	var rendererOptions = {
 	    draggable: true,
-	    suppressMarkers: true
+	    suppressMarkers: true,
+	    polylineOptions: {strokeColor:'#c0392b'}
 	};
 
 	var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
@@ -20,6 +21,39 @@ app.controller('mapCtrl', ['$scope', 'ReceiveService', function ($scope, Receive
 	var objects;			// table of ambulances
 	var poznan = new google.maps.LatLng(52.411629, 16.933938);
 
+	var featureOpts = [
+    	{
+		    featureType: 'road',
+		    elementType: 'all',
+		    stylers: [
+		        { color: '#1abc9c' },
+		        { saturation: 60 },
+		        { lightness: -20 },
+        		{ visibility: 'simplified' },
+	      	]
+    	},
+    	{
+		    featureType: 'road',
+		    elementType: 'labels',
+		    stylers: [
+        		{ visibility: 'off' },
+	      	]
+    	},
+    	{
+		    featureType: 'landscape',
+		    elementType: 'labels',
+		    stylers: [
+        		{ visibility: 'off' },
+	      	]
+    	},
+    	{
+		    featureType: 'landscape',
+		    stylers: [
+		        { color: '#ecf0f1' },
+	      	]
+    	},
+  	];
+
 	function initialize() { 
 
 		setInterval( function() {
@@ -27,14 +61,29 @@ app.controller('mapCtrl', ['$scope', 'ReceiveService', function ($scope, Receive
 		    for (var i = 0; i < objects.length; i++) {
 		        placeAmbulance(i, new google.maps.LatLng(objects[i].latitude, objects[i].longitude));
 		    }
-		}, 5000);
+		}, 2000);
 
-		var mapOptions = {
-		    zoom: 13,
-		    center: poznan
+		var styledMapOptions = {
+		    name: 'Custom Style'
 		};
 
+		var MY_MAPTYPE_ID = 'custom_style';
+		
+		var mapOptions = {
+		    zoom: 13,
+		    center: poznan,
+		    mapTypeControlOptions: {
+		      	mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
+		    },
+		    mapTypeId: MY_MAPTYPE_ID
+		};
+
+	  	var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
+
 		map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+		
+		map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
+
 		directionsDisplay.setMap(map);
 		// directionsDisplay.setPanel(document.getElementById('directionsPanel'));
 
@@ -56,9 +105,10 @@ app.controller('mapCtrl', ['$scope', 'ReceiveService', function ($scope, Receive
 			objects[i].marker.setMap(null);
 			objects[i].marker = null;
 		}
-		objects[i].marker = new google.maps.Marker({
+		objects[i].marker = objects[i].status == "offline" ? null : new google.maps.Marker({
 		    position: location, 
-		    map: map
+		    map: map,
+      		icon: objects[i].state == "free" ? 'images/spotlight-poi-green.png' : 'images/spotlight-poi-blue.png',
 		});
 		// console.log(objects[i].marker);
 	}
@@ -71,6 +121,7 @@ app.controller('mapCtrl', ['$scope', 'ReceiveService', function ($scope, Receive
 		marker = new google.maps.Marker({
 		    position: location, 
 		    map: map,
+      		icon: 'images/spotlight-poi.png',
 		    draggable: true,
 		    animation: google.maps.Animation.BOUNCE
 		});
@@ -81,28 +132,30 @@ app.controller('mapCtrl', ['$scope', 'ReceiveService', function ($scope, Receive
 		var calc = 0;
 
 		for (var i = 0; i < objects.length; i++) {
-			var request = {
-			    origin: objects[i].marker.position,
-			    destination: marker.position,
-			    travelMode: google.maps.TravelMode.DRIVING
-			};
-			directionsService.route(request, function(response, status) {
-			    if (status == google.maps.DirectionsStatus.OK) {
-		  			calc++;
-			        var myroute = response.routes[0];
-			        var distance = 0;
-			        for (var i = 0; i < myroute.legs.length; i++) {
-					    distance += myroute.legs[i].distance.value;
-					}
+			if(objects[i].marker) {
+				var request = {
+				    origin: objects[i].marker.position,
+				    destination: marker.position,
+				    travelMode: google.maps.TravelMode.DRIVING
+				};
+				directionsService.route(request, function(response, status) {
+				    if (status == google.maps.DirectionsStatus.OK) {
+			  			calc++;
+				        var myroute = response.routes[0];
+				        var distance = 0;
+				        for (var i = 0; i < myroute.legs.length; i++) {
+						    distance += myroute.legs[i].distance.value;
+						}
 
-		  			console.log(distance + " < " + best.distance);
-		  			if(distance < best.distance || !best.distance) {
-		  				best.distance = distance;
-		  				best.route = response;
-		  				directionsDisplay.setDirections(best.route);
-		  			}
-			    }
-		  	});
+			  			console.log(distance + " < " + best.distance);
+			  			if(distance < best.distance || !best.distance) {
+			  				best.distance = distance;
+			  				best.route = response;
+			  				directionsDisplay.setDirections(best.route);
+			  			}
+				    }
+			  	});
+			}
 		}
 
 
