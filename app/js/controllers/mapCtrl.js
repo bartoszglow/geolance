7bf5f7840d26d0099d519db03858ddcf8b80a9e3
joinee <bartosz.glow@gmail.com>
2014-05-19 09:53:29 +0200

    Search box

--- app/js/controllers/mapCtrl.js
+++ app/js/controllers/mapCtrl.js
@@ -19,6 +19,7 @@ app.controller('mapCtrl', ['$scope', 'ReceiveService', function ($scope, Receive
 	var map;
 	var marker;				// touch event marker
 	var objects;			// table of ambulances
+	var old_objects;
 	var poznan = new google.maps.LatLng(52.411629, 16.933938);
 
 	var featureOpts = [
@@ -57,10 +58,12 @@ app.controller('mapCtrl', ['$scope', 'ReceiveService', function ($scope, Receive
 	function initialize() { 
 
 		setInterval( function() {
+			old_objects = objects ? objects : ReceiveService.getData( properties );
 		    objects = ReceiveService.getData( properties );
 		    for (var i = 0; i < objects.length; i++) {
 		        placeAmbulance(i, new google.maps.LatLng(objects[i].latitude, objects[i].longitude));
 		    }
+		    calcRoute();
 		}, 2000);
 
 		var styledMapOptions = {
@@ -87,9 +90,9 @@ app.controller('mapCtrl', ['$scope', 'ReceiveService', function ($scope, Receive
 		directionsDisplay.setMap(map);
 		// directionsDisplay.setPanel(document.getElementById('directionsPanel'));
 
-		google.maps.event.addListener(directionsDisplay, 'directions_changed', function() {    
-			calcRoute();
-		});
+		// google.maps.event.addListener(directionsDisplay, 'directions_changed', function() {    
+		// 	calcRoute();
+		// });
 
 		google.maps.event.addListener(map, 'click', function(event) {
 		   	placeMarker(event.latLng);
@@ -108,19 +111,18 @@ app.controller('mapCtrl', ['$scope', 'ReceiveService', function ($scope, Receive
 	}
 
 	function placeAmbulance(i, location) {
-		if(objects[i].marker) {
-			if(objects[i].marker.position.A == location.A && objects[i].marker.position.k == location.k) {
-				return false;
-			}
-			objects[i].marker.setMap(null);
-			objects[i].marker = null;
-		}
-		objects[i].marker = objects[i].status == "offline" ? null : new google.maps.Marker({
+		objects[i].marker = objects[i].status == "offline" ? undefined : new google.maps.Marker({
 		    position: location, 
 		    map: map,
       		icon: objects[i].state == "free" ? 'images/spotlight-poi-green.png' : 'images/spotlight-poi-blue.png',
 		});
-		// console.log(objects[i].marker);
+		setTimeout(function(){
+			if(old_objects[i].marker) {
+				old_objects[i].marker.setMap(null);
+				old_objects[i].marker = undefined;
+			}
+		},50);
+		
 	}
 
 	function placeMarker(location) {
@@ -143,7 +145,7 @@ app.controller('mapCtrl', ['$scope', 'ReceiveService', function ($scope, Receive
 		var calc = 0;
 
 		for (var i = 0; i < objects.length; i++) {
-			if(objects[i].marker) {
+			if(objects[i].marker && marker) {
 				var request = {
 				    origin: objects[i].marker.position,
 				    destination: marker.position,
@@ -151,7 +153,6 @@ app.controller('mapCtrl', ['$scope', 'ReceiveService', function ($scope, Receive
 				};
 				directionsService.route(request, function(response, status) {
 				    if (status == google.maps.DirectionsStatus.OK) {
-			  			calc++;
 				        var myroute = response.routes[0];
 				        var distance = 0;
 				        for (var i = 0; i < myroute.legs.length; i++) {
@@ -160,27 +161,19 @@ app.controller('mapCtrl', ['$scope', 'ReceiveService', function ($scope, Receive
 
 			  			// console.log(distance + " < " + best.distance);
 			  			if(distance < best.distance || !best.distance) {
-			  				best.distance = distance;
-			  				best.route = response;
-			  				directionsDisplay.setDirections(best.route);
+				  			best.distance = distance;
+				  			best.route = response;
+				  			directionsDisplay.setDirections(best.route);
 			  			}
 				    }
 			  	});
+			} else {
+				calc++;
+				if( calc == objects.length ) {
+					directionsDisplay.set('directions', null);
+				}
 			}
 		}
-
-
-		// computeTotalDistance(temp_route);
-
-
-
-
-			// directionsService.route(request, function(response, status) {
-			//     if (status == google.maps.DirectionsStatus.OK) {
-			//         directionsDisplay.setDirections(response);
-			//     }
-		 //  	});
-		
 	}
 
 	function computeTotalDistance(result) {
@@ -208,7 +201,9 @@ app.controller('mapCtrl', ['$scope', 'ReceiveService', function ($scope, Receive
 	});
 
 	
-
+	$('#search-input').click(function(){
+		$('#search-input').parent().toggleClass('focus');
+	});
 
 
 }]);
--- app/js/services/ReceiveService.js
+++ app/js/services/ReceiveService.js
@@ -8,7 +8,7 @@ app.factory("ReceiveService", ['$location', '$http', function($location, $http) 
 	       		table = data;
 
 		        for (var i = 0; i < table.length; i++) {
-		            table[i].status = (new Date() - new Date(table[i].lastLogin)) > 10000 ? "offline" : "online"; 
+		            table[i].status = (new Date() - new Date(table[i].lastLogin)) > 60000 ? "offline" : "online"; 
 		            table[i].state = table[i].status == "offline" ? "-" : table[i].state == 0 ? "bussy" : "free";
 		        }
 
